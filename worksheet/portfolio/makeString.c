3090afc6259d7ccd4c1890bb0b2fe6881ec3f2f5
vlpl5634AS <vlpl5634@leeds.ac.uk>
2026-02-21 14:28:38 +0000

    portiflio done

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -14,8 +14,9 @@ int main( int argc, char **argv ) {
     buffer[0] = '\0'; //initialise an empty string
     
     // process the command-line data using appropriate string functions
+    // loop trhough the arguments enterd till we reach the end start at 1 becasue we want to skip "./makestring", which is argv[0]
     for (int i = 1; i <argc; i++){
-        strcat(buffer, argv[i]);  // concatenate the arguments wuth the buffer
+        strcat(buffer, argv[i]);  // concatenate the arguments with the buffer we made
 
         if (i < argc - 1) {
                 strcat(buffer, "-"); // add a hyphen in between the different arguments as long as its not last argument
